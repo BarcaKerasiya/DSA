f613ff4fbe1266592bab37c390f5ea11cccca6b3
Arjun Ahir <arjunahir1718@gmail.com>
2024-01-15 11:06:53 +0530

    1 is completed

--- 1.js
+++ 1.js
@@ -2,9 +2,16 @@
 
 // Multiply array with number
 
-// let numbers = [1,2,5,4,6]
-// let num = 5
+let numbers = [1,2,5,4,6]
+let num = 5
+
+
+let mul = [];
+for (let i = 0; i < numbers.length; i++) {
+  mul.push(numbers[i] * num);
+}
+
+console.log(mul);
 
 // output [5,10,25,20,30]
 
-console.log('test')
