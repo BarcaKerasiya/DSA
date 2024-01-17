e9ae4531100ebe7a47bf2916489be04acf3908e2
Arjun Ahir <arjunahir1718@gmail.com>
2024-01-17 11:12:27 +0530

    3 is completed

--- 3.js
+++ 3.js
@@ -6,3 +6,19 @@
 // addition(-3, -6) ➞ -9
 
 // addition(7, 3) ➞ 10
+
+function addition(number1 , number2) {
+    return number1 + number2
+}
+
+console.log(addition(3,2))
+console.log(addition(-3,-6))
+console.log(addition(7,3))
+
+//2
+
+const addi = (number1,number2) => number1+number2
+
+console.log(addi(3,2))
+console.log(addi(-3,-6))
+console.log(addi(7,3))
