6994ea56f74bb170ffd4076006d97a789b39392a
Arjun Ahir <arjunahir1718@gmail.com>
2024-01-18 10:31:43 +0530

    4 is competed

--- 4.js
+++ 4.js
@@ -1,3 +1,15 @@
 //Write a JavaScript function that reverses a number.
 //Example x = 32243;
 //Expected Output : 34223
+
+
+
+function reverseNum(number) {
+    const reversedString = number.toString().split('').reverse().join('');
+    return parseInt(reversedString, 10);
+  }
+  
+  const x = 32243;
+  const reversedX = reverseNum(x);
+  console.log(reversedX); 
+  
