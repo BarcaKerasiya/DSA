57253ba88664c12abf7a106cc8f0ca82c1a12634
Arjun Ahir <arjunahir1718@gmail.com>
2024-01-21 20:31:00 +0530

    7 is completed

--- 7.js
+++ 7.js
@@ -3,3 +3,10 @@
 // let arr = [43, "what", 9, true, "cannot", false, "be", 3, true];
 // Example output: 
 // true 3.5  be  false cannot  true 9 what 43 OR each item on a new line
+
+let arr = [43, "what", 9, true, "cannot", false, "be", 3, true];
+
+for (let i = arr.length - 1; i >= 0; i--) {
+    console.log(arr[i]);
+}
+
