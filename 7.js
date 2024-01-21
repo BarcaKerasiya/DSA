3692556b6724dd846e7ed162d32e4309c4d13cba
Kerasiya Vishnu <vishnuk@ziguts.com>
2024-01-21 13:50:04 +0000

    answer 7

--- 7.js
+++ 7.js
@@ -1,5 +1,13 @@
 //Using a for loop output the elements in reverse order
 
 // let arr = [43, "what", 9, true, "cannot", false, "be", 3, true];
-// Example output: 
+// Example output:
 // true 3.5  be  false cannot  true 9 what 43 OR each item on a new line
+
+const reverserOrder = (arr) => {
+  return arr.reverse().join(" ");
+};
+
+console.log(
+  reverserOrder([43, "what", 9, true, "cannot", false, "be", 3, true])
+);
