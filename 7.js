800443626c1c8863c2c2a865657fc5268efcc521
Kerasiya Vishnu <vishnuk@ziguts.com>
2024-01-21 14:00:06 +0000

    7 complete

--- 7.js
+++ 7.js
@@ -5,7 +5,12 @@
 // true 3.5  be  false cannot  true 9 what 43 OR each item on a new line
 
 const reverserOrder = (arr) => {
-  return arr.reverse().join(" ");
+  //   return arr.reverse().join(" ");
+  let str = "";
+  for (let i = arr.length - 1; i >= 0; i--) {
+    str = str + arr[i] + " ";
+  }
+  return str;
 };
 
 console.log(
