158786a7995b1407121e903927817832ac928345
Arjun Ahir <arjunahir1718@gmail.com>
2024-01-23 10:31:27 +0530

    8 is completed

--- 8.js
+++ 8.js
@@ -5,3 +5,11 @@
 // let arr_4    = [8, 1, 9];
 // Example output:
 // [12, 7, 16]
+
+let arr_3 = [4, 6, 7];
+let arr_4 = [8, 1, 9];
+
+let resultArray = arr_3.map((element, index) => element + arr_4[index]);
+
+console.log(resultArray)
+
