79c8aec9b616c05acba90ad5424b93e92a360600
Arjun Ahir <arjunahir1718@gmail.com>
2024-01-23 10:38:11 +0530

    9 is completed

--- 9.js
+++ 9.js
@@ -1,7 +1,17 @@
-Given a string change the every second letter to an uppercase ‘Z’. Assume
-there are no space.
+// Given a string change the every second letter to an uppercase ‘Z’. Assume
+// there are no space.
 
 let str1 = "javascript";  
 // Example output: 
 // jZvZsZrZpZ OR each letter on a new line
 // HINT: You can use  if((i+1) % 2 == 0) to check for even indexes 
+
+
+
+for (let i = 0; i < str1.length; i++) {
+    if ((i + 1) % 2 === 0) {
+        console.log('Z');
+    } else {
+        console.log(str1[i]);
+    }
+}
